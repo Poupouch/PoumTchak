1e1f3001a52e514c3d4d4a1b899745355622b497
Salmon <psalmon@hugo.ai>
2018-10-02 21:34:33 -0700

    Fix the Search Bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Button from "./components/Button";
-import Link from "./components/Link";
 import SearchBar from "./components/SearchBar";
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link url="Hi Kenneth" text = "Click here"/>
-        <Button title="Hi Milio" />
-        <Button title="Hi Pol" />
-        <SearchBar search="Oh" />
+        <SearchBar />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
 
-
 export default class SearchBar extends Component {
-    render() {
-        return <input value={this.props.search}
-        onChange={this.handleInputChange}> </input>
-    }
+  constructor(props) {
+    super(props); // IGNORE THIS
+    this.handleInputChange = this.handleInputChange.bind(this); // IGNORE THIS
+
+    this.state = { search: "" };
+  }
+
+  handleInputChange(event) {
+    this.setState({ search: event.target.value });
+  }
+
+  render() {
+    const { search } = this.state;
+    return <input value={search} onChange={this.handleInputChange} />;
+  }
 }
